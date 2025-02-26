7c88a85dca69ddbae1a92ce90915362da2684224
Laura Epie <lauramuke12@gmail.com>
2025-02-26 19:43:35 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "terraform.state"
+    key            = "tooling/terraform.state"
     bucket         = "mamuks-terraform-backend-bucket"
     region         = "us-west-2"
     dynamodb_table = "mamuks-state-locking"
